4e4fdb391e949af511812c89f2a866414035fc20
gh0x7 <107593451+adaomajor@users.noreply.github.com>
2026-03-20 20:49:36 +0100

    Fix syntax error in Debug function exception handling

--- App/Core/Utils.php
+++ App/Core/Utils.php
@@ -5,7 +5,7 @@
     function Debug($message){
         $env = getenv("DEBUG");
         if(isset($env) && $env == "DEBUG" || $env == "TRUE"){
-            return throw new Exception($message);
+            throw new Exception($message);
         }else{
             //echo "PROBABLY IN PRODUCTION";
             //return ;
